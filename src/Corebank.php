8fe1911ca58d55d097ebc77662f83a8cc1746b46
Tee <admin@jquerytips.com>
2017-01-09 22:14:17 +0700

    corebank

--- src/Corebank.php
+++ src/Corebank.php
@@ -9,12 +9,49 @@ use GuzzleHttp\Exception\RequestException;
 
 class Corebank
 {
+    /**
+     * Guzzle client.
+     * 
+     * @var \GuzzleHttp\Client
+     */
     protected $client;
 
+    /**
+     * Api endpoint.
+     * 
+     * @var string
+     */
     protected $endpoint;
 
+    /**
+     * Request timeout.
+     * 
+     * @var integer
+     */
     protected $timeout = 30;
 
+    /**
+     * Access token.
+     * 
+     * @var string
+     */
+    protected $accessToken;
+
+    /**
+     * Request headers.
+     * 
+     * @var array
+     */
+    protected $headers = [
+        'Content-Type' => 'application/x-www-form-urlencoded',
+        'Accept' => 'application/json'
+    ];
+
+    /**
+     * Construct.
+     * 
+     * @param array $config 
+     */
     public function __construct($config)
     {
         $this->client = new GuzzleHttp\Client();
@@ -25,20 +62,34 @@ class Corebank
     }
 
     /**
-     * Post transactions to corebank.
-     *
-     * @param  array $transactions
-     * @return array
+     * Set access token.
      */
-    // public function createTransactions($transactions)
-    // {
-    //     $response = $this->api('POST', '/transactions', [
-    //         'json' => $transactions
-    //     ]);
+    public function setAccessToken($token)
+    {
+        $this->accessToken = $token;
 
-    //     return $response;
-    // }
+        $this->headers = array_merge($this->headers, [
+            'Authorization' => 'Bearer '.$token
+        ]);
 
+        return $this;
+    }
+
+    /**
+     * Return access token.
+     * 
+     * @return string
+     */
+    public function getAccessToken()
+    {
+        return $this->accessToken;
+    }
+
+    /**
+     * Set Timeput.
+     * 
+     * @param integer $timeout 
+     */
     public function setTimeout($timeout)
     {
         $this->timeout = $timeout;
@@ -46,12 +97,48 @@ class Corebank
         return $this;
     }
 
+    /**
+     * Set headers.
+     * 
+     * @param array $headers 
+     */
+    public function setHeaders($headers = [])
+    {
+        $this->headers = array_merge($this->headers, $headers);
+
+        return $this;
+    }
+
+    /**
+     * Set a header.
+     * 
+     * @param string $key   
+     * @param stainr $value 
+     */
+    public function setHeader($key, $value) 
+    {
+        $this->setHeaders([$key => $value]);
+
+        return $this;
+    }
+
+    /**
+     * Make request.
+     * 
+     * @param  string $method     
+     * @param  string $path       
+     * @param  array  $formParams 
+     * @return \GuzzleHttp\Psr7\Response          
+     */
     public function api($method = 'GET', $path, array $formParams = [])
     {
         $method = strtoupper($method);
         $path = ltrim($path, '/');
 
         $endpoint = $this->endpoint.'/'.$path;
+
+        $headers = $this->headers;
+        dump($headers);
 
         $parameters = [
             'headers' => [
